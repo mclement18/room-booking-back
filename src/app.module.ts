2b2d0db747f51a17dec56956caad3590d3fae924
Mathieu Clément <mathieu.clement@iit.it>
2023-06-20 14:15:47 +0200

    Configure TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+
+function typeOrmConfigurationProvider(
+  configService: ConfigService,
+): TypeOrmModuleOptions {
+  return {
+    type: 'postgres',
+    host: configService.get<string>('database.host'),
+    port: configService.get<number>('database.port'),
+    username: configService.get<string>('database.username'),
+    password: configService.get<string>('database.password'),
+    database: configService.get<string>('database.database'),
+    entities: [],
+    synchronize: true,
+  };
+}
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfigurationProvider,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
