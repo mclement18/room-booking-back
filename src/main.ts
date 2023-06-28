7baa58dee98dd29726e2ed03fb0b6313698c5862
Mathieu Clément <mathieu.clement@iit.it>
2023-06-28 10:06:51 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.ALLOWED_ORIGINS?.split(',') || [] });
   await app.listen(3000);
 }
 bootstrap();
