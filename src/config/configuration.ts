c81b76b27027b3415a779447feee0bd06838937c
Mathieu Clément <mcfly1893@gmail.com>
2023-07-04 14:36:51 +0200

    Fix config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,5 +5,6 @@ export default () => ({
     port: parseInt(process.env.DB_PORT || '', 10) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
